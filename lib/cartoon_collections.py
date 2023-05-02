82ba31c1dafd28efc87a91854f58879fd3417fca
Sarah Jones <sarahrjones8@gmail.com>
2023-05-01 21:12:39 -0400

    complete lab

--- lib/cartoon_collections.py
+++ lib/cartoon_collections.py
@@ -1,11 +1,35 @@
-def roll_call_dwarves():
-    pass
+def roll_call_dwarves(dwarves):
+    # for index, dwarf in enumerate(dwarves):
+    #     print(f"{index + 1}. {dwarf}")
+    i = 1
+    for dwarf in dwarves:
+        print(f"{i}. {dwarf}")
+        i += 1
 
-def summon_captain_planet():
-    pass
+def summon_captain_planet(planeteer_calls):
+    # call_list = []
+    # for call in planeteer_calls:
+    #     call_list.append(call.title() + "!")
+    # return call_list
+    return[call.title() + "!" for call in planeteer_calls]
 
-def long_planeteer_calls():
-    pass
+def long_planeteer_calls(calls):
+    for call in calls:
+        if len(call) < 4:
+            return True
+        else:
+            return False
 
-def find_the_cheese():
-    pass
+
+
+def find_the_cheese(strings):
+
+    cheeses = ["cheddar", "gouda", "camembert"]
+
+    for string in strings:
+        if string in cheeses:
+            return string
+        # else:
+        #     return None
+            
+            
